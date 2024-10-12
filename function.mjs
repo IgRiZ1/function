d1316cc3a9bdb35905acb8f1e723778bd851ec2b
IgRiZ1 <dalil.blh@outlook.be>
2024-10-12 10:02:11 +0200

    Update function.mjs

--- function.mjs
+++ function.mjs
@@ -5,11 +5,15 @@ import { log } from 'node:console';
 const userInput = readline.createInterface({input, output});
 
 
-function maalDrie(num){
-    return(num * 3 );
+ function tvserie(serie1){
+    if(serie1 == 'dragonball'){
+        console.log('Je gekozen TV-serie vind in japan ');
+    } else {
+        console.log('Sorry, ik weet niet waar de serie '+ serie1 +' zich afspeelt');
+        
+    }
+    return(serie1)
+ }
+    tvserie('simpson')
     
-}
-
-
-console.log(maalDrie(102));
-process.exit()
+    process.exit()
